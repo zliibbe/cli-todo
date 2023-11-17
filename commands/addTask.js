395cd837547a406dc9eb41c9bdcb12592a0a268b
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2023-11-17 10:34:37 -0700

    feat: Add ability to add tasks and return array of todos

--- commands/addTask.js
+++ commands/addTask.js
@@ -13,5 +13,18 @@ async function input() {
   return answers
 }
 
-const output = await input()
-console.log(output)
+const askQuestions = async() => {
+
+  const todoArray = []
+  let loop = false
+
+  do {
+    const userRes = await input()
+    todoArray.push(userRes)
+    const confirmQ = await inquirer.prompt([{ name: 'confirm', message: 'Do you want to add more tasks?', type: 'confirm' }])
+    if(confirmQ.confirm) loop=true
+    else loop = false
+  } while(loop)
+
+  return todoArray
+}
