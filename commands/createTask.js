d20139015f3f7d69735cfc7f84c7b69860f4e467
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2023-11-17 14:39:45 -0700

    Update code style

--- commands/createTask.js
+++ commands/createTask.js
@@ -22,8 +22,11 @@ const askQuestions = async() => {
     const userRes = await input()
     todoArray.push(userRes)
     const confirmQ = await inquirer.prompt([{ name: 'confirm', message: 'Do you want to add more tasks?', type: 'confirm' }])
-    if(confirmQ.confirm) loop=true
-    else loop = false
+    if(confirmQ.confirm) {
+      loop=true
+    } else {
+      loop=false
+    } 
   } while(loop)
 
   return todoArray
